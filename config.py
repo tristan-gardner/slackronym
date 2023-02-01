24f1c5094ef0a6a8545e0233cfd0ee8ec175ddfa
tristan-gardner <trgardner123@gmail.com>
2023-02-01 14:23:51 -0500

    make it work

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 from key_vault import get_secret
 
+KEY_VAULT_NAME = "KV-ROOPUSE-SLACKBOT-P"
+
 
 def get_setting(name: str, default: str = None):
     try:
-        val = get_secret(name)["value"]
+        val = get_secret(KEY_VAULT_NAME, name)["value"]
         return val
     except Exception:
         return default
 
 
-DB_PASSWORD = get_setting("DB_PASSWORD")
-DB_USER = get_setting("DB_USER")
-DB_HOST = get_setting("DB_HOST")
-DB_NAME = get_setting("DB_NAME")
+DB_PASSWORD = get_setting("DbPassword")
+DB_USER = get_setting("DbUser")
+DB_HOST = get_setting("DbHost")
+DB_NAME = get_setting("DbName")
--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
-from typing import Dict, List
-from urllib.parse import quote, unquote
+import re
+from typing import Dict
+from urllib.parse import unquote
 
 from django.http import HttpResponse
 from rest_framework import views
 
 from main.manager import create_definition, get_definitions
-from main.models import Definition
 from main.serializers import CreateDefintionSerializer
 
 
@@ -14,26 +14,50 @@ def convert_x_www_form_data_to_dict(data: str) -> Dict[str, str]:
     return {k: unquote(v) for k, v in [x.split("=") for x in data.split("&")]}
 
 
+# format incoming slack message
+# slack send text with plus signs for spaces
+def format_incoming_slack_message(text: str) -> str:
+    spaced_def = re.sub(r"(?<!\\)\+", " ", text)
+    return spaced_def.replace("\\+", "+").replace("\+", "+")
+
+
 # slack route
 class DefineView(views.APIView):
+    def post(self, request):
+        return self._handle_request(request)
+
     def get(self, request):
+        return self._handle_request(request)
+
+    def _handle_request(self, request):
         request_data = convert_x_www_form_data_to_dict(request.body.decode("utf-8"))
         if "text" not in request_data:
             return HttpResponse("")
 
-        definitions: List[Definition] = []
-
         argument = request_data["text"]
         args = argument.split("=", maxsplit=1)
+        response_text = ""
+
         if len(args) == 1:
             term = args[0]
+            response_text = f"Definition(s) for {term}:\n"
             definitions = get_definitions(term)
+            if not definitions:
+                response_text = (
+                    f"No definition found for {term} create one with /define {term}=[definition]"
+                )
+            if len(definitions) == 1:
+                response_text += definitions[0].definition
+            else:
+                response_text += "\n".join(
+                    f"{idx+1}. {d.definition}" for idx, d in enumerate(definitions)
+                )
 
         elif len(args) == 2:
             term, new_def = args
-            definitions = [create_definition(term, new_def)]
-
-        response_text = "\n".join(quote(d.definition) for d in definitions)
+            formated_def = format_incoming_slack_message(new_def)
+            new_def = create_definition(term, formated_def)
+            response_text = f"Added definition for {term}: {new_def.definition}"
 
         return HttpResponse(response_text)
 
